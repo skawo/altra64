e40439315a4d2c115ee9fa62779a7fb8358fd35a
networkfusion <robin+github@networkfusion.co.uk>
2019-08-14 16:46:03 +0100

    add more detailed version string

--- src/version.c
+++ src/version.c
@@ -1,13 +1,24 @@
 //
-// Copyright (c) 2017 The Altra64 project contributors
+// Copyright (c) 2019 The Altra64 project contributors
 // See LICENSE file in the project root for full license information.
 //
 
 #include "version.h"
 
-#define ALTRA64_VERSION "0.1.8.6.1.3"
+#define ALTRA64_VERSION "0.1.8.6.1.4"
+
+#ifndef OS_BUILD_VERSION
+  #define OS_BUILD_VERSION "0"  //populate from CI-CD pipeline Version
+#endif
+#ifndef DEBUG //populate from makefile
+  #define OS_DEBUG_VERSION ""  
+#else
+  #define OS_DEBUG_VERSION "-DBG"
+#endif
 
 const char* Altra64_GetVersionString(void)
 {
-    return ALTRA64_VERSION;
+    char *versionStr[30];
+     sprintf(versionStr, "%s.%s%s", ALTRA64_VERSION, OS_BUILD_VERSION, OS_DEBUG_VERSION);
+    return versionStr;
 }
