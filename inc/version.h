8590a0620504abda0bac202957a6cf466b37d1fe
networkfusion <robin+github@networkfusion.co.uk>
2019-08-14 16:49:45 +0100

    corrections to previous commit

--- inc/version.h
+++ inc/version.h
@@ -6,6 +6,17 @@
 #ifndef _VERSION_H
 #define _VERSION_H
 
+#define ALTRA64_VERSION "0.1.8.6.1.4"
+
+#ifndef OS_BUILD_VERSION
+  #define OS_BUILD_VERSION "0"  //populate from CI-CD pipeline Version
+#endif
+#ifndef DEBUG //populate from makefile
+  #define OS_DEBUG_VERSION ""  
+#else
+  #define OS_DEBUG_VERSION "-DBG"
+#endif
+
 const char* Altra64_GetVersionString(void);
 
 #endif
--- src/version.c
+++ src/version.c
@@ -4,17 +4,7 @@
 //
 
 #include "version.h"
-
-#define ALTRA64_VERSION "0.1.8.6.1.4"
-
-#ifndef OS_BUILD_VERSION
-  #define OS_BUILD_VERSION "0"  //populate from CI-CD pipeline Version
-#endif
-#ifndef DEBUG //populate from makefile
-  #define OS_DEBUG_VERSION ""  
-#else
-  #define OS_DEBUG_VERSION "-DBG"
-#endif
+#include <stdio.h>
 
 const char* Altra64_GetVersionString(void)
 {
